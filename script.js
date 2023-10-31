dae022b427915a6f9c94ec54e8d142de2395d005
andyoriginal <37127028+andyoriginal@users.noreply.github.com>
2023-10-30 20:24:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+
+/*
+let js = "amazing";
+//if (js === "amazing") alert("JavaScript is here!");
+
+let number = 40 + 8 + 23 - 10;
+console.log(number)
+
+let firstName = "BingoBongo";
+
+console.log(firstName);
+
+let country = "USA",
+    continent = "NA",
+    population = "332 million";
+
+console.log(country, continent, population);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'Yes';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+
+let age = 35;
+age = 36;
+
+const birthyear = 1987;
+
+const job; 
+
+//math operators
+const CY = 2037;
+const ageJonas = CY - 1991;
+const ageSarah = CY - 2018;
+
+console.log(ageJonas);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Scmedtmann';
+console.log(firstName + " " + lastName);
+
+//assignment operators
+let x = 10 + 5; //15
+x += 10; // += means x = x + 10 
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 
+x--; // x = x -1 
+
+console.log(x);
+
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+console.log(CY - 1991 > CY - 2018);
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018); // order of operators 
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+*/
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthyear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthyear) + ' year old ' + job + '!';
+
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthyear}, year old ${job} ! `;
+
+console.log(jonasNew);
+console.log(`String
+multiple
+lines`)
+
+
+
+
+
+
+
+
+
+
+
