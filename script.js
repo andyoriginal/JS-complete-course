b159bb60cdb43638bdbdf9f4409c09563e8359dd
andyoriginal <37127028+andyoriginal@users.noreply.github.com>
2023-11-02 13:59:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ if (favourite === 23) { //always try to use === operator // 22 === 23 -> FALSE
     console.log('Number is not 23 or 7');
 }
 
-*/
+
 
 const hasDriversLicense = true; // A
 const hasGoodVision = true; //B
@@ -214,10 +214,37 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
     console.log('Somebody else should drive');
 }
+*/
 
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
 
+// console.log(scoreDolphins, scoreKoalas);
 
+// if (scoreDolphins > scoreKoalas) {
+//     console.log("Dolphins win the trophy!");
+// } else if (scoreKoalas > scoreDolphins) {
+//     console.log("Koalas win the trophy!");
+// } else if (scoreDolphins === scoreKoalas) {
+//     console.log("Both win the trophy");
+// } else {
+//     console.log("Nobody wins the trophy")
+// }
 
+const scoreDolphins = (97 + 112 + 102) / 3;
+const scoreKoalas = (109 + 95 + 83) / 3;
+
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log("Dolphins win the trophy!");
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log("Koalas win the trophy!");
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log("Both win the trophy");
+} else {
+    console.log("Nobody wins the trophy")
+}
 
 
 
