7208bee808467c84ae39a4aef0a8380d7f92b679
andyoriginal <37127028+andyoriginal@users.noreply.github.com>
2023-11-05 20:25:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -244,7 +244,7 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 } else {
     console.log("Nobody wins the trophy")
 }
-*/
+
 
 const day = prompt('What day is it?');
 
@@ -272,4 +272,30 @@ switch (day) {
 }
 
 3 + 4 // this is an expression 
+1991
+true && false && !false
+
+if (23 > 10) {
+    const str = '23 is bigger';
+}
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`)
+*/
+
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine') :
+//     console.log('I like to drink water');
+
+const drink = age >= 18 ? 'Wine' : 'water'
+console.log(drink)
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+
+console.log(drink2);
 
