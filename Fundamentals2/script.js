bf3c2e1a46b51921dad90776b3d1aabe8def2ebc
andyoriginal <37127028+andyoriginal@users.noreply.github.com>
2023-12-28 21:07:22 -0600

    Update script.js

--- Fundamentals2/script.js
+++ Fundamentals2/script.js
@@ -45,19 +45,60 @@
 // console.log(age1, age2);
 
 // Arrow function
-const calcAge3 = birthyear => 2037 - birthyear;
-const age3 = calcAge3(1991);
-console.log(age3);
+// const calcAge3 = birthyear => 2037 - birthyear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
 
-const yearsUntilRetirement = (birthyear, firstName) => {
-    const age = 2037 - birthyear;
-    const retirement = 65 - age;
-    // return retirement;
-    return `${firstName} retires in ${retirement} years`;
+// const yearsUntilRetirement = (birthyear, firstName) => {
+//     const age = 2037 - birthyear;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1994, 'Bob'));
+
+// function cutPieces(fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutPieces(apples);
+//     const orangePieces = cutPieces(oranges);
+
+//     const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+//     return juice;
+// }
+
+// console.log(fruitProcessor(5, 6));
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(2, 8, 16);
+
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win ${avgDolphins} vs ${avgKoalas}`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win ${avgKoalas} vs ${avgDolphins} `);
+    } else {
+        console.log("No team wins")
+    }
 }
 
-console.log(yearsUntilRetirement(1991, 'Jonas'));
-console.log(yearsUntilRetirement(1994, 'Bob'));
+checkWinner(scoreDolphins, scoreKoalas);
+
+
+
+
+
+
+
+
 
 
 
